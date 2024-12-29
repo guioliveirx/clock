8df7056ebdf8e23d956ecfa40310fd32bc55872f
Guilherme <150380352+guioliveirx@users.noreply.github.com>
2024-12-29 16:12:05 -0300

    feat: create accessibility and logo of page

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="description" content="Projeto de um relógio digital feito com ReactJs"/>
+        <title>ClockReact</title>
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
